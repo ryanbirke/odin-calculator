973a2a8547946e326aaf63b4bc62a3542a9db3b0
Ryan Birke <8440910+ryanbirke@users.noreply.github.com>
2024-11-30 12:18:56 -0500

    Create simple functions for 4 main operations

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function addTwoNumbers(num1, num2){
+    return num1 + num2;
+};
+
+function subtractTwoNumbers(num1, num2){
+    return num1 - num2;
+};
+
+function multiplyTwoNumbers(num1, num2){
+    return num1 * num2;
+};
+
+function divideTwoNumbers(num1, num2){
+    return num1/num2;
+};
